c3068860ae326d0125d81c3fa2b2034f4901aa75
Oktora Kevin Arigi <oktora.kevinarigi@ai.astra.co.id>
2019-06-18 19:34:37 +0700

    publish

---
+++ doc/precache-manifest.b5a873c1791b2c4254d25897f6895346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421610335a24220b43f030f49efdbe8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f0788becf4ebd939482",
+    "url": "/static/css/2.865957ec.chunk.css"
+  },
+  {
+    "revision": "7f62a81f0f39d6b663c7",
+    "url": "/static/css/main.4e6b226f.chunk.css"
+  },
+  {
+    "revision": "8f0788becf4ebd939482",
+    "url": "/static/js/2.39e774f7.chunk.js"
+  },
+  {
+    "revision": "7f62a81f0f39d6b663c7",
+    "url": "/static/js/main.3e704738.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
